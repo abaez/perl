4a9773bd7f6c9e35d741e9510ccffaa23b7b01d5
Martin Persia <mpersia@gmail.com>
2012-04-16 00:41:02 -0400

    Update yet again

--- martin/eg2.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl
-    @ME= `perldoc -u -f atan2`;   #using the array ME with `` instead of '' opens the "perldoc" program
-    foreach (@ME) {		#For loop 
-      s/\w<([^>]+)>/\U$1/g;
-      print;
-    }
-
-
