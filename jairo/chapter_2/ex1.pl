f23625554287cb5dbc5b720f50c42bcde16f6213
Jairo <rosariojairo@gmail.com>
2012-04-23 04:00:08 -0400

    Chapter 2 update

---
+++ jairo/chapter_2/ex1.pl
@@ -0,0 +1,5 @@
+#!/usr/bin/perl
+#chapter 2 exercise 1 ಠ_ಠ
+$pai = 3.141592654;   #another way of saying pie.
+$enshu = 2 * $pai * 12.5;   #another way of saying circumfurence plus a formula.
+print " The enshu of a saakuru of radius 12.5 is $enshu.\n";   # very very answer
